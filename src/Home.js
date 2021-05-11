8523fd22769cbe9b23c04f0764390f82572a0a68
tejash-b <52689752+tejash-b@users.noreply.github.com>
2021-05-11 17:10:57 +0530

    img reverted

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import img from './img/home.jpg';
 const Home = () => {
     return ( 
         <div className="home">
-        <img src="https://images.unsplash.com/photo-1596633608169-2ee5f4ed60e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="home"></img>
+        <img src= {img} alt="home"></img>
         <div className="img-content">
             
             Welcome,<br/>
