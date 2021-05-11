c3a6646f38cff0d6ce068b7c86ccb44bc85b5603
tejash-b <52689752+tejash-b@users.noreply.github.com>
2021-05-11 16:43:07 +0530

    classname

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return ( 
         <div className="home">
         <img src= {img} alt="home"></img>
-        <div class="img-content">
+        <div className="img-content">
             
             Welcome,<br/>
             This is just a simple project to handel the student details<br/> where Admin can View, Add, Edit and Delete the Student Details.
